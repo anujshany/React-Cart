0289fc0974c773e0673b139b32409ae86385155c
anujshany <anujshany@gmail.com>
2021-08-23 17:02:51 +0530

    Event Listners Implementation

---
+++ src/CartItem.js
@@ -0,0 +1,62 @@
+import React from 'react'
+
+class CartItem extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            price:999,
+            title:'Mobile Phone',
+            qty:1,
+            img:''
+        }
+    }
+    IncreaseQuantity = () => {
+       console.log('this.state', this.state) 
+    }
+    render(){
+        const{price,title,qty} = this.state;
+        return(
+            <div className='cart-item'>
+                <div className='left-block'>
+                    <img style={styles.image}/>
+                </div>
+                <div className='right-block'>
+                    <div style={{fontSize:25}}>{title}</div>
+                    <div style={{color:'#777'}}>Rs {price}</div>
+                    <div style={{color:'#777'}}>{qty}</div>
+                    <div className='cart-item-actions'>
+                        {/* Buttons */}
+                        <img
+                            alt='increase' 
+                            className="action-icons"
+                            src="https://image.flaticon.com/icons/png/512/1828/1828817.png"
+                            onClick={this.IncreaseQuantity}
+                        />
+                        <img 
+                            alt='decrease'
+                            className="action-icons" 
+                            src="https://image.flaticon.com/icons/png/512/334/334047.png"
+                            onClick={this.DecreaseQuantity}
+                        />
+                        <img 
+                            alt='delete' 
+                            className="action-icons" 
+                            src="https://image.flaticon.com/icons/png/512/3221/3221897.png"
+                        />
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+const styles = {
+    image:{
+        height:110,
+        width:110,
+        borderRadius:4,
+        background:'#ccc'
+    }
+}
+
+export default CartItem;
